00fe2fd3ed7a3f7b7c2c89f51684411284ba7ec5
marty808 <62924159+marty808@users.noreply.github.com>
2021-08-25 20:16:12 +0200

    change umask

--- entry.sh
+++ entry.sh
@@ -20,11 +20,11 @@ rclone config create WEBDAV webdav vendor nextcloud url ${WEBDAV_HOST}/${WEBDAV_
 
 if [ ${RCLONE_INIT} ]; then
    echo "copy initial data from Webdav ${WEBDAV_HOST}/${WEBDAV_PATH} to /data"
-   rclone --umask $UMASK copy WEBDAV:/ /data
+   rclone copy --umask=$UMASK WEBDAV:/ /data
 fi
 
 # build command for RCLONE
-cmd="/bin/rclone ${RCLONE_MODE} --umask $UMASK /data WEBDAV:/"
+cmd="/bin/rclone ${RCLONE_MODE} --umask=$UMASK /data WEBDAV:/"
 
 echo "Setup backup cron job with cron schedule: ${CRON_SChEDULE}"
 echo "${CRON_SCHEDULE} /usr/bin/flock -n /var/run/backup.lock ${cmd} >> /var/log/cron.log 2>&1" > /var/spool/cron/crontabs/root
